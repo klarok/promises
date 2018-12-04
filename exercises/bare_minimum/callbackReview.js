075fa7f1852faa5c491835ccee2ba82a1f09632c
klarok <karaylu@gmail.com>
2018-12-03 17:19:54 -0800

    passed getStatusCode

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -7,12 +7,25 @@ var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+  fs.readFile(filePath, (err, firstLine) => {
+    if (firstLine !== undefined) {
+      let line = firstLine.toString('utf8').split('\n');
+      arguments[1](err, line[0]);
+    } else {
+      arguments[1](err, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url) {
-  // TODO
+  request(url, (error, response) => {
+    if (error) {
+      arguments[1](error, response);
+    } else {
+      arguments[1](error, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
